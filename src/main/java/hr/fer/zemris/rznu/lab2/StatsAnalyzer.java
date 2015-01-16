2d1156b36d3053c6f45fd475a4d91f8094945116
bpervan <branimir.pervan@gmail.com>
2015-01-16 15:37:33 +0100

    Added class structure

---
+++ src/main/java/hr/fer/zemris/rznu/lab2/StatsAnalyzer.java
@@ -0,0 +1,10 @@
+package hr.fer.zemris.rznu.lab2;
+
+/**
+ * Created by Branimir on 16.1.2015..
+ */
+public class StatsAnalyzer {
+    public static void main(String[] args){
+        
+    }
+}
---
+++ src/main/java/hr/fer/zemris/rznu/lab2/browsers/BrowserAccessMap.java
@@ -0,0 +1,7 @@
+package hr.fer.zemris.rznu.lab2.browsers;
+
+/**
+ * Created by Branimir on 16.1.2015..
+ */
+public class BrowserAccessMap {
+}
